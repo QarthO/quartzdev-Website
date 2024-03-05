716c78a3b0f1143be4c24f989f6ab5ff9075f106
QarthO <qartho@gmail.com>
2024-03-04 19:25:24 -0500

    fix routes using client api

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -1,5 +1,11 @@
+import BrowserOnly from "@docusaurus/BrowserOnly"
+
 const url = "https://discord.gg/QM7dG78rNW"
 
 export default function permanentRedirect(){
-  window.location.assign(url)
+  <BrowserOnly>
+    <script>
+      {window.location.assign(url)}
+    </script>
+  </BrowserOnly>
 }
--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,5 +1,11 @@
+import BrowserOnly from "@docusaurus/BrowserOnly"
+
 const url = "https://www.paypal.com/donate/?hosted_button_id=MP5VDJYTS6RQJ"
 
 export default function permanentRedirect(){
-  window.location.assign(url)
+  <BrowserOnly>
+    <script>
+      {window.location.assign(url)}
+    </script>
+  </BrowserOnly>
 }
