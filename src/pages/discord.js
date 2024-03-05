ac23654b88a52a8dd72dfb0c829620d9b97fa9d7
QarthO <qartho@gmail.com>
2024-03-04 19:13:10 -0500

    added proper redirects

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -1,25 +1,5 @@
-import React from "react";
-import Layout from "@theme/Layout";
-import Link from '@docusaurus/Link';
+const url = "https://discord.gg/QM7dG78rNW"
 
-export default function Test(){
-    return (
-        <Layout title="Hello" description="Hello React Page">
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '50vh',
-              fontSize: '20px',
-            }}>
-
-          <Link
-            className="button button--secondary button--lg"
-            to="https://discord.gg/QM7dG78rNW">
-            Sorry broken page route, click here to go to my discord
-          </Link>
-          </div>
-        </Layout>
-      );
+export default function permanentRedirect(){
+  window.location.assign(url)
 }
--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,25 +1,5 @@
-import React from "react";
-import Layout from "@theme/Layout";
-import Link from '@docusaurus/Link';
+const url = "https://www.paypal.com/donate/?hosted_button_id=MP5VDJYTS6RQJ"
 
-export default function Test(){
-    return (
-        <Layout title="Hello" description="Hello React Page">
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '50vh',
-              fontSize: '20px',
-            }}>
-
-          <Link
-            className="button button--secondary button--lg"
-            to="https://www.paypal.com/donate/?hosted_button_id=MP5VDJYTS6RQJ">
-            Sorry broken page route, click here to go to my paypal
-          </Link>
-          </div>
-        </Layout>
-      );
+export default function permanentRedirect(){
+  window.location.assign(url)
 }
